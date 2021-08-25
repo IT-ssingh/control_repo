019474c24a25a7fec303361aaf433349d0eab2a5
Shivam Singh <73770451+IT-ssingh@users.noreply.github.com>
2021-08-25 04:10:34 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockergent 
-  dockeragent:node {'web.puppet.vm':}
-  dockeragent:node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
