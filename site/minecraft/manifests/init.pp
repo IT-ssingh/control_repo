c75e9731090a7071989485796b06c9390189b57a
Shivam Singh <73770451+Shivam-S-Singh@users.noreply.github.com>
2021-08-27 05:42:35 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,12 +8,11 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
   }
   
-  java::download { 'jdk16' :
+  java::adopt { 'jdk16' :
   ensure  => 'present',
-  java_se => 'jdk',
-  url     => 'https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz',
-  basedir => '/usr/java',
-  }
+  version => '16',
+  java => 'jdk',
+ }
  
   file {'/opt/minecraft/eula.txt':
     ensure => file,
