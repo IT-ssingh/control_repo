42df37ee8a47b691985c09afdd680d3f8d3bfb47
Shivam Singh <73770451+Shivam-S-Singh@users.noreply.github.com>
2021-08-27 05:53:16 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,11 +8,10 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
   }
   
-  java::adopt { 'jdk16' :
-  ensure  => 'present',
-  version => '16',
-  java => 'jdk',
- }
+  $java_packages = [ 'java-1.8.0-openjdk', 'java-1.8.0-openjdk-headless', 'java-1.8.0-openjdk-devel' ]
+  package: { $java_packages:
+  ensure => '1.8.0.201.b09'
+  }
  
   file {'/opt/minecraft/eula.txt':
     ensure => file,
